2ac0433c77469621526a88710ab1bd8e8eac91f4
arfost <clement.chevalier@cirruseo.com>
2018-01-09 15:35:30 +0100

    new readingbar bookmarklets

---
+++ bm/readingBar.js
@@ -0,0 +1,7 @@
+let child = document.createElement("div");
+child.style.position = "fixed"
+child.style.top = "15em"
+child.style.width = "100vw"
+child.style.height = "0.1em"
+child.style.backgroundColor = "red"
+document.body.appendChild(child)
