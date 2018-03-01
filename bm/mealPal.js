c9b08a384bb10df37b4c75ea8914224845210a4b
Chevalier clement <clement.chevalier@cirruseo.com>
2018-03-01 17:31:15 +0100

    mealpal new version

--- bm/mealPal.js
+++ bm/mealPal.js
@@ -1,2 +1,5 @@
 document.querySelector('#dialog-rate-meal').style.display='none';
 document.querySelector('#content-overlay').style.display = 'none';
+
+document.querySelector('#mp-meal-list > div').style.overflow = 'scroll';
+document.querySelector('#mp-meal-list > div').style.display = 'flex';
